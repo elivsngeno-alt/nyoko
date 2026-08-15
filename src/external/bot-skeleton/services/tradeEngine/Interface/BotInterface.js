d816bf7e303e88432cc1a06e086fcfccf6ae6636
Mr Duke <133217491+DukeNyamasege@users.noreply.github.com>
2026-08-15 09:44:17 +0300

    Support runtime market switching for uploaded bots

--- src/external/bot-skeleton/services/tradeEngine/Interface/BotInterface.js
+++ src/external/bot-skeleton/services/tradeEngine/Interface/BotInterface.js
@@ -9,6 +9,27 @@ const getBotInterface = tradeEngine => {
         start: (...args) => tradeEngine.start(...args),
         stop: (...args) => tradeEngine.stop(...args),
         purchase: contract_type => tradeEngine.purchase(contract_type),
+        changeSymbol: async symbol => {
+            if (!symbol || symbol === 'disable') return tradeEngine.symbol;
+
+            await tradeEngine.watchTicks(symbol);
+
+            if (tradeEngine.options) {
+                tradeEngine.options = { ...tradeEngine.options, symbol };
+            }
+            if (tradeEngine.tradeOptions) {
+                tradeEngine.tradeOptions = { ...tradeEngine.tradeOptions, symbol };
+            }
+            if (Array.isArray(tradeEngine.initArgs) && tradeEngine.initArgs.length >= 2) {
+                tradeEngine.initArgs = [
+                    tradeEngine.initArgs[0],
+                    { ...tradeEngine.initArgs[1], symbol },
+                    ...tradeEngine.initArgs.slice(2),
+                ];
+            }
+
+            return symbol;
+        },
         getAskPrice: contract_type => Number(getProposal(contract_type, tradeEngine).ask_price),
         getPayout: contract_type => Number(getProposal(contract_type, tradeEngine).payout),
         getPurchaseReference: () => tradeEngine.getPurchaseReference(),
