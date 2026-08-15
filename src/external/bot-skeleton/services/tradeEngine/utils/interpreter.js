14f8a22151a9719b97fef6d25d7cf1d850d71733
Mr Duke <133217491+DukeNyamasege@users.noreply.github.com>
2026-08-15 09:44:41 +0300

    Expose async market switching to Blockly bots

--- src/external/bot-skeleton/services/tradeEngine/utils/interpreter.js
+++ src/external/bot-skeleton/services/tradeEngine/utils/interpreter.js
@@ -133,6 +133,11 @@ const Interpreter = () => {
         );
         js_interpreter.setProperty(
             pseudo_bot_interface,
+            'changeSymbol',
+            createAsync(js_interpreter, bot_interface.changeSymbol)
+        );
+        js_interpreter.setProperty(
+            pseudo_bot_interface,
             'sellAtMarket',
             createAsync(js_interpreter, bot_interface.sellAtMarket)
         );
